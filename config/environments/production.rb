ab47afbe994ba1642e2ca2deadc40c552fd2bca3
Erick Bernardo <erickvictor@gmail.com>
2019-04-13 13:56:01 -0400

    Set up Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "easy-lives_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'easy-lives1.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'easylives.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    authentication:       :plain,
+    enable_starttls_auto: true,
+    domain:               'easylives.herokuapp.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD']
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
